ff548db8c1de1bf2f99ec906e710bd9bfc83c163
Vincent Nawrocki <vincent@Vincents-MacBook-Pro.local>
2018-04-23 20:14:28 +0200

    Added fashion with Boostrap on Gallery page

--- apigatewayclient.js
+++ apigatewayclient.js
@@ -1,7 +1,6 @@
 var api_url = "http://amazon-api-gateway-url.com/update-me!";
 
 
-
 function getAjax(url, success) {
     var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
     xhr.open('GET', url);
@@ -14,18 +13,36 @@ function getAjax(url, success) {
 
 getAjax(api_url, function(data){ 
 
-for (var i = 0;i<data.length;i++){
-	var div = document.createElement('div');
-	var img = document.createElement('img');
-	var p = document.createElement('p');
-	p.innerHTML = 	"<b>Upload date: </b>"+data[i].eventTime+ 
-					"<br><b>filename :</b> "+data[i].filename+ 
-					"<br><b>URL :</b> <a href='"+data[i].url+"'>"+data[i].url+"</a>";
-	img.src = data[i].url;
-	img.title = data[i].filename;
-	div.appendChild(img);
-	div.appendChild(p);
-	document.body.appendChild(div);
-}
+	for (var i = 0;i<data.length;i++){
+		var divCard = document.createElement('div');
+		divCard.className = "card";
+		divCard.style.cssText = "width: 18rem;";
+
+		var cardTitle = document.createElement('h5');
+		cardTitle.className = "card-title";
+		cardTitle.innerHTML = "Filename : "+data[i].filename;
+
+		var p = document.createElement('p');
+		p.className = "card-text";
+		p.innerHTML = 	"<b>Upload date: </b>"+data[i].eventTime+ 
+						"<br><b>URL :</b> <a href='"+data[i].url+"'>"+data[i].url+"</a>";
+
+
+		var img = document.createElement('img');
+		img.src = data[i].url;
+		img.alt = data[i].filename;
+		img.className = "card-img-top";
+
+		var divCardBody = document.createElement('div');
+		divCardBody.className = "card-body";
+
+		CardBody.appendChild(cardTitle);
+		CardBody.appendChild(p);
+
+		divCard.appendChild(img);
+		divCard.appendChild(CardBody);
+
+		document.body.appendChild(divCard);
+	}
 });
 document.getElementById("apigwurl").innerHTML = api_url;
