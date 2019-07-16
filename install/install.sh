d139bcbffad59376c0bb9f42554a0ace8b74f3ea
Chimon S <chimons@users.noreply.github.com>
2019-07-16 12:55:30 +0200

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -1,7 +1,8 @@
 sudo yum update –y
-sudo yum install -y httpd24 php56
+sudo yum install -y httpd php56
+sudo amazon-linux-extras install -y php7.2
 maxsize=25M
-sudo sed -i 's/upload_max_filesize = .*/upload_max_filesize = '${maxsize}'/' /etc/php.ini
+sudo sed -i 's/upload_max_filesize = .*/upload_max_filesize = '${maxsize}'/' /e$
 sudo service httpd start
 sudo chkconfig httpd on
 sudo groupadd www
@@ -18,8 +19,8 @@ mv awslab-master/* .
 rm awslab-master.zip
 rm -R awslab-master/ install/
 printf '\033[0;32m  Installation of the AWS LAB environment completed\e[m\n';
-printf '\033[0;32m  Now, please update the index.php file with your actual S3 Bucket name and region.\e[m\n';
-printf '\033[0;32m  You will also need to provide your API Gateway URL in apigatewayclient.js\e[m\n';
-printf '\033[0;32m Then you will be able to access your application thru your browser at : \033[1;32m';
+printf '\033[0;32m  Now, please update the index.php file with your actual S3 B$
+printf '\033[0;32m  You will also need to provide your API Gateway URL in apiga$
+printf '\033[0;32m Then you will be able to access your application thru your b$
 curl -s http://169.254.169.254/latest/meta-data/public-hostname
 printf '\e[m\n\n'
