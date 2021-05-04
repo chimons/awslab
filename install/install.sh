fdbc01aaee9e83abdb8227ec10f565194a5d9187
Chimon S <chimons@users.noreply.github.com>
2021-05-04 19:42:10 +0200

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -17,9 +17,9 @@ find /var/www -type f -exec sudo chmod 0664 {} +
 cd /var/www/html
 wget https://github.com/chimons/awslab/archive/refs/heads/master.zip
 unzip -q master.zip
-mv master/* .
+mv awslab-master/* .
 rm master.zip
-rm -R master/ install/
+rm -R awslab-master/ install/
 printf '\033[0;32m  Installation of the AWS LAB environment completed\e[m\n';
 printf '\033[0;32m  Now, please update the index.php file with your actual S3 Bucket name and region.\e[m\n';
 printf '\033[0;32m  You will also need to provide your API Gateway URL in apigatewayclient.js\e[m\n';
